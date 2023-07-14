9d85db29a4660cdb6e52debb474c89f7e3c169a5
Jacó Magalhães <jacomagalhaes@gmail.com>
2023-07-14 04:11:08 -0300

    GOF - Builder

---
+++ src/GOF/04 - Builder/director/Director.ts
@@ -0,0 +1,33 @@
+import IBuilder from "../builders/IBuilder";
+import Engine from "../components/Engine";
+import Transmission from "../components/Transmission";
+import VehicleType from "../components/VehicleType";
+import Wheel from "../components/Wheel";
+
+export default class Director {
+    constructor(private builder: IBuilder){
+
+    }
+
+     constructSedanCar(){
+        this.builder.setVehicleType(VehicleType.SEDAN);
+        this.builder.setSeats(4);
+        this.builder.setTransmission(Transmission.AUTOMATIC);
+        this.builder.setEngine(new Engine(1600));
+        this.builder.addWheel(new Wheel(15));
+        this.builder.addWheel(new Wheel(15));
+        this.builder.addWheel(new Wheel(15));
+        this.builder.addWheel(new Wheel(15));
+     }
+
+     constructTruck(){
+        this.builder.setVehicleType(VehicleType.TRUCK);
+        this.builder.setSeats(2);
+        this.builder.setTransmission(Transmission.AUTOMATIC);
+        this.builder.setEngine(new Engine(1600));
+        this.builder.addWheel(new Wheel(30));
+        this.builder.addWheel(new Wheel(30));
+        this.builder.addWheel(new Wheel(30));
+        this.builder.addWheel(new Wheel(30));
+     }
+}
